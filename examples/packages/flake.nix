b408c2f27c12142d4d506f34ca539838531d6a39
Ratson <github@ratson.name>
2024-10-29 21:42:22 +0800

    chore: add devShells example

--- examples/packages/flake.nix
+++ examples/packages/flake.nix
@@ -11,6 +11,10 @@
 
       perSystem = { pkgs, ... }: {
         packages.haloha = pkgs.hello;
+
+        devShells.default = pkgs.mkShell {
+          packages = [ inputs.self.packages.${pkgs.system}.greet ];
+        };
       };
     };
 }
