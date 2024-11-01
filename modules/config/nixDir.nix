851e5d9f4abe81ac4b66c4db33206e431eca9cba
Ratson <github@ratson.name>
2024-11-01 19:03:02 +0800

    feat: ignore loading path with _

--- modules/config/nixDir.nix
+++ modules/config/nixDir.nix
@@ -1,42 +1,39 @@
 { config, lib, ... }:
 
 let
-  inherit (builtins) attrValues foldl' mapAttrs pathExists readDir;
-
-  inherit (lib) removeSuffix flip nameValuePair mkOption pipe remove types;
-
   loadDir = dir:
     let
       toEntry = path: type:
         let
           fullPath = dir + /${path};
-          name = removeSuffix ".nix" path;
+          name = lib.removeSuffix ".nix" path;
         in
-        if type == "directory" then
-          nameValuePair name (loadDir fullPath)
-        else if type == "regular" then
-          nameValuePair name fullPath
+        if lib.hasPrefix "_" path then null
+        else if type == "directory" then
+          lib.nameValuePair name (loadDir fullPath)
+        else if type == "regular" && lib.hasSuffix ".nix" path then
+          lib.nameValuePair name fullPath
         else null;
     in
-    pipe dir [
-      (x: if pathExists x then readDir x else { })
-      (mapAttrs toEntry)
-      attrValues
-      (remove null)
-      (flip foldl' { }
+    lib.pipe dir [
+      (x: lib.optionalAttrs (builtins.pathExists x) (builtins.readDir x))
+      (builtins.mapAttrs toEntry)
+      builtins.attrValues
+      (lib.remove null)
+      (lib.flip builtins.foldl' { }
         (acc: { name, value }:
           acc // { ${name} = value; }))
     ];
 in
 {
   options = {
-    nixDir = mkOption {
-      type = types.path;
+    nixDir = lib.mkOption {
+      type = lib.types.path;
       default = config.src + /nix;
     };
 
-    nixDirEntries = mkOption {
-      type = types.raw;
+    nixDirEntries = lib.mkOption {
+      type = lib.types.raw;
       readOnly = true;
     };
   };
