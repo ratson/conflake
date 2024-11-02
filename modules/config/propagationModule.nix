37b0f8a10a6e4fc0fdf397ed0257cd2f0104b6d1
Ratson <github@ratson.name>
2024-11-03 04:07:55 +0800

    refactor: utilize mkMerge

--- modules/config/propagationModule.nix
+++ modules/config/propagationModule.nix
@@ -1,49 +1,58 @@
-# This provides a module that can be added to module systems nested inside of
-# conflake, for example NixOS or home-manager configurations.
-{ lib, config, conflake, moduleArgs, inputs, outputs, ... }:
+{ config, lib, conflake, moduleArgs, inputs, outputs, ... }:
 
 let
   inherit (builtins) mapAttrs;
-  inherit (lib) mkOption optional optionalAttrs;
+  inherit (lib) mkIf mkMerge mkOption mkOrder optional optionalAttrs;
   inherit (conflake) selectAttr;
   inherit (conflake.types) module;
-  flakeConfig = config;
+
+  rootConfig = config;
 in
 {
   options.propagationModule = mkOption {
     type = module;
     internal = true;
+    description = ''
+      A module that can be added to module systems nested inside of Conflake,
+      for example NixOS or home-manager configurations.
+    '';
   };
 
-  config.propagationModule = { lib, pkgs, options, config, ... }:
+  config.propagationModule = { config, options, lib, pkgs, ... }:
     let
       inherit (pkgs.stdenv.hostPlatform) system;
     in
     {
-      config = (optionalAttrs (options ? nixpkgs) {
-        nixpkgs = (optionalAttrs (options ? nixpkgs.overlays) {
-          # Forward overlays to NixOS/home-manager configurations
-          overlays = lib.mkOrder 10
-            (flakeConfig.withOverlays ++ [ flakeConfig.packageOverlay ]);
+      _file = ./propagationModule.nix;
+
+      config = mkMerge [
+        {
+          # Give access to conflake module args under `flake` arg.
+          # Also include inputs'/outputs' which depend on `pkgs`.
+          _module.args.flake = {
+            inputs' = mapAttrs (_: selectAttr system) inputs;
+            outputs' = selectAttr system outputs;
+          } // moduleArgs;
+        }
+        (mkIf (options ? nixpkgs) {
+          nixpkgs = mkMerge [
+            (mkIf (options ? nixpkgs.overlays) {
+              # Forward overlays to NixOS/home-manager configurations
+              overlays = mkOrder 10
+                (rootConfig.withOverlays ++ [ rootConfig.packageOverlay ]);
+            })
+            (mkIf (options ? nixpkgs.config) {
+              # Forward nixpkgs.config to NixOS/home-manager configurations
+              inherit (rootConfig.nixpkgs) config;
+            })
+          ];
         })
-        // (optionalAttrs (options ? nixpkgs.config) {
-          # Forward nixpkgs.config to NixOS/home-manager configurations
-          inherit (flakeConfig.nixpkgs) config;
-        });
-      })
-      // (optionalAttrs (options ? home-manager.sharedModules) {
-        # Propagate module to home-manager when using its nixos module
-        home-manager.sharedModules =
-          optional (! config.home-manager.useGlobalPkgs)
-            [ flakeConfig.propagationModule ];
-      })
-      // {
-        # Give access to conflake module args under `flake` arg.
-        # Also include inputs'/outputs' which depend on `pkgs`.
-        _module.args.flake = {
-          inputs' = mapAttrs (_: selectAttr system) inputs;
-          outputs' = selectAttr system outputs;
-        } // moduleArgs;
-      };
+        (optionalAttrs (options ? home-manager.sharedModules) {
+          # Propagate module to home-manager when using its nixos module
+          home-manager.sharedModules =
+            optional (! config.home-manager.useGlobalPkgs)
+              [ rootConfig.propagationModule ];
+        })
+      ];
     };
 }
