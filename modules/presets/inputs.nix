9581621a698bd0a898af0d23e22d720ea3749890
Ratson <github@ratson.name>
2025-01-15 23:01:56 +0800

    feat: add auto inputs priority config

--- modules/presets/inputs.nix
+++ modules/presets/inputs.nix
@@ -11,22 +11,32 @@ let
   inherit (lib)
     mkEnableOption
     mkIf
+    mkOption
     mkOverride
     pipe
+    types
     ;
   inherit (conflake.flake) lock2inputs;
+
+  cfg = config.presets.inputs;
 
   flakeLockExists = (config.srcEntries."flake.lock" or "") == "regular";
 in
 {
-  options.presets.inputs.enable = mkEnableOption "auto inputs" // {
-    default = config.presets.enable;
+  options.presets.inputs = {
+    enable = mkEnableOption "auto inputs" // {
+      default = config.presets.enable;
+    };
+    priority = mkOption {
+      type = types.int;
+      default = 950;
+    };
   };
 
-  config = mkIf (config.inputs == null && flakeLockExists) {
+  config = mkIf (cfg.enable && config.inputs == null && flakeLockExists) {
     finalInputs = pipe (src + /flake.lock) [
       lock2inputs
-      (mapAttrs (_: mkOverride 950))
+      (mapAttrs (_: mkOverride cfg.priority))
     ];
   };
 }
