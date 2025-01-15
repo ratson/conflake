1a269b1be4fc0d973bbb3ca3622d4e2bb1e0210a
Ratson <github@ratson.name>
2025-01-15 22:44:46 +0800

    feat: export lib.flake.lock2inputs

--- lib/flake.nix
+++ lib/flake.nix
@@ -1,8 +1,16 @@
 { lib, ... }:
 
 let
-  inherit (builtins) substring;
-  inherit (lib) concatStrings;
+  inherit (builtins)
+    fromJSON
+    head
+    isString
+    mapAttrs
+    readFile
+    substring
+    tail
+    ;
+  inherit (lib) concatStrings fix pipe;
 
   mkVersion' =
     prefix: input:
@@ -16,6 +24,62 @@ let
 in
 {
   inherit mkVersion';
+
+  lock2inputs =
+    src:
+    let
+      json = pipe src [
+        readFile
+        fromJSON
+      ];
+      inherit (json) nodes root;
+
+      getInputName =
+        base: ref:
+        let
+          next = getInputName root nodes.${base}.inputs.${head ref};
+        in
+        if isString ref then
+          ref
+        else if ref == [ ] then
+          base
+        else
+          getInputName next (tail ref);
+
+      getInput = ref: resolved.${getInputName root ref};
+
+      fetchNode = node: fetchTree (node.info or { } // removeAttrs node.locked [ "dir" ]);
+
+      resolveFlakeNode =
+        node:
+        fix (
+          self:
+          let
+            sourceInfo = fetchNode node;
+            outPath = sourceInfo + (if node.locked ? dir then "/${node.locked.dir}" else "");
+            inputs = (mapAttrs (_: getInput) (node.inputs or { })) // {
+              inherit self;
+            };
+            outputs = (import (outPath + "/flake.nix")).outputs inputs;
+          in
+          outputs
+          // sourceInfo
+          // {
+            _type = "flake";
+            inherit
+              inputs
+              outPath
+              outputs
+              sourceInfo
+              ;
+          }
+        );
+
+      resolveNode = node: if node.flake or true then resolveFlakeNode node else fetchNode node;
+
+      resolved = mapAttrs (_: resolveNode) nodes;
+    in
+    mapAttrs (_: getInput) nodes.${root}.inputs;
 
   /**
     Create a version string from flake input.
--- modules/presets/inputs.nix
+++ modules/presets/inputs.nix
@@ -1,80 +1,22 @@
 {
   config,
   lib,
+  conflake,
   src,
   ...
 }:
 
 let
-  inherit (builtins)
-    fromJSON
-    head
-    isString
-    mapAttrs
-    readFile
-    tail
-    ;
+  inherit (builtins) mapAttrs;
   inherit (lib)
-    fix
     mkEnableOption
     mkIf
     mkOverride
     pipe
     ;
+  inherit (conflake.flake) lock2inputs;
 
   flakeLockExists = (config.srcEntries."flake.lock" or "") == "regular";
-
-  flakeLock = src + /flake.lock;
-
-  lock2inputs =
-    { nodes, root, ... }:
-    let
-      getInputName =
-        base: ref:
-        let
-          next = getInputName root nodes.${base}.inputs.${head ref};
-        in
-        if isString ref then
-          ref
-        else if ref == [ ] then
-          base
-        else
-          getInputName next (tail ref);
-
-      getInput = ref: resolved.${getInputName root ref};
-
-      fetchNode = node: fetchTree (node.info or { } // removeAttrs node.locked [ "dir" ]);
-
-      resolveFlakeNode =
-        node:
-        fix (
-          self:
-          let
-            sourceInfo = fetchNode node;
-            outPath = sourceInfo + (if node.locked ? dir then "/${node.locked.dir}" else "");
-            inputs = (mapAttrs (_: getInput) (node.inputs or { })) // {
-              inherit self;
-            };
-            outputs = (import (outPath + "/flake.nix")).outputs inputs;
-          in
-          outputs
-          // sourceInfo
-          // {
-            _type = "flake";
-            inherit
-              inputs
-              outPath
-              outputs
-              sourceInfo
-              ;
-          }
-        );
-
-      resolveNode = node: if node.flake or true then resolveFlakeNode node else fetchNode node;
-
-      resolved = mapAttrs (_: resolveNode) nodes;
-    in
-    mapAttrs (_: getInput) nodes.${root}.inputs;
 in
 {
   options.presets.inputs.enable = mkEnableOption "auto inputs" // {
@@ -82,9 +24,7 @@ in
   };
 
   config = mkIf (config.inputs == null && flakeLockExists) {
-    finalInputs = pipe flakeLock [
-      readFile
-      fromJSON
+    finalInputs = pipe (src + /flake.lock) [
       lock2inputs
       (mapAttrs (_: mkOverride 950))
     ];
