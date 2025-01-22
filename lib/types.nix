c0c0fa6ea809feeaf5d304487fe94b926c2944db
Ratson <github@ratson.name>
2025-01-22 12:55:56 +0800

    refactor: remove rec usages

--- lib/types.nix
+++ lib/types.nix
@@ -16,6 +16,7 @@ let
   inherit (lib)
     composeManyExtensions
     filter
+    fix
     getFiles
     getValues
     isDerivation
@@ -93,220 +94,236 @@ let
       listToAttrs
     ];
 in
-rec {
-  mkCheck =
-    src:
-    mkOptionType {
-      name = "check";
-      description = pipe drv [
-        (coercedTo' stringLike (abort ""))
-        (optionDescriptionPhrase (class: class == "noun" || class == "composite"))
-        (targetDesc: "${targetDesc} or function that evaluates to it")
-      ];
-      descriptionClass = "composite";
-      check = x: isFunction x || drv.check x || stringLike.check x;
-      merge =
-        loc: defs: pkgs:
-        let
-          coerceFunc = self.mkCheck (last loc) pkgs src;
-          targetType = coercedTo' stringLike coerceFunc drv;
-        in
-        pipe defs [
-          (map (fn: {
-            inherit (fn) file;
-            value = if isFunction fn.value then fn.value pkgs else fn.value;
-          }))
-          (mergeDefinitions loc targetType)
-          (x: x.mergedValue)
+fix (
+  types':
+  let
+    inherit (types')
+      coercedTo'
+      drv
+      function
+      loader
+      nullable
+      outputsValue
+      path
+      pathTree
+      stringLike
+      ;
+  in
+  {
+    mkCheck =
+      src:
+      mkOptionType {
+        name = "check";
+        description = pipe drv [
+          (coercedTo' stringLike (abort ""))
+          (optionDescriptionPhrase (class: class == "noun" || class == "composite"))
+          (targetDesc: "${targetDesc} or function that evaluates to it")
         ];
+        descriptionClass = "composite";
+        check = x: isFunction x || drv.check x || stringLike.check x;
+        merge =
+          loc: defs: pkgs:
+          let
+            coerceFunc = self.mkCheck (last loc) pkgs src;
+            targetType = coercedTo' stringLike coerceFunc drv;
+          in
+          pipe defs [
+            (map (fn: {
+              inherit (fn) file;
+              value = if isFunction fn.value then fn.value pkgs else fn.value;
+            }))
+            (mergeDefinitions loc targetType)
+            (x: x.mergedValue)
+          ];
+      };
+
+    coercedTo' =
+      coercedType: coerceFunc: finalType:
+      (coercedTo coercedType coerceFunc finalType)
+      // {
+        merge =
+          loc: defs:
+          let
+            coerceVal = val: if finalType.check val then val else coerceFunc val;
+          in
+          finalType.merge loc (map (def: def // { value = coerceVal def.value; }) defs);
+      };
+
+    drv = mkOptionType {
+      name = "drv";
+      description = "derivation";
+      descriptionClass = "noun";
+      check = isDerivation;
+      merge = mergeOneOption;
     };
 
-  coercedTo' =
-    coercedType: coerceFunc: finalType:
-    (coercedTo coercedType coerceFunc finalType)
-    // {
-      merge =
-        loc: defs:
-        let
-          coerceVal = val: if finalType.check val then val else coerceFunc val;
-        in
-        finalType.merge loc (map (def: def // { value = coerceVal def.value; }) defs);
+    fileset = mkOptionType {
+      name = "fileset";
+      description = "fileset";
+      descriptionClass = "noun";
+      check = x: isPath x || x._type or null == "fileset";
     };
 
-  drv = mkOptionType {
-    name = "drv";
-    description = "derivation";
-    descriptionClass = "noun";
-    check = isDerivation;
-    merge = mergeOneOption;
-  };
+    function = mkOptionType {
+      name = "function";
+      description = "function";
+      descriptionClass = "noun";
+      check = isFunction;
+      merge = mergeOneOption;
+    };
 
-  fileset = mkOptionType {
-    name = "fileset";
-    description = "fileset";
-    descriptionClass = "noun";
-    check = x: isPath x || x._type or null == "fileset";
-  };
+    loadable = types.unspecified // {
+      name = "loadble";
+    };
 
-  function = mkOptionType {
-    name = "function";
-    description = "function";
-    descriptionClass = "noun";
-    check = isFunction;
-    merge = mergeOneOption;
-  };
+    loader = submodule (
+      { name, ... }:
+      {
+        options = {
+          enable = mkEnableOption "${name} loader" // {
+            default = true;
+          };
+          collect = mkOption {
+            type = functionTo (lazyAttrsOf raw);
+            default = collect;
+          };
+          match = mkOption {
+            type = functionTo bool;
+            default = self.matchers.dir;
+          };
+          load = mkOption {
+            type = functionTo (lazyAttrsOf raw);
+            default = _: { };
+          };
+          loaders = mkOption {
+            type = lazyAttrsOf loader;
+            default = { };
+          };
+        };
+      }
+    );
 
-  loadable = types.unspecified // {
-    name = "loadble";
-  };
+    loaders = lazyAttrsOf loader;
 
-  loader = submodule (
-    { name, ... }:
-    {
-      options = {
-        enable = mkEnableOption "${name} loader" // {
-          default = true;
-        };
-        collect = mkOption {
-          type = functionTo (lazyAttrsOf raw);
-          default = collect;
-        };
-        match = mkOption {
-          type = functionTo bool;
-          default = self.matchers.dir;
-        };
-        load = mkOption {
-          type = functionTo (lazyAttrsOf raw);
-          default = _: { };
-        };
-        loaders = mkOption {
-          type = lazyAttrsOf loader;
-          default = { };
+    module = mkOptionType {
+      name = "module";
+      description = "module";
+      descriptionClass = "noun";
+      check = x: isPath x || isFunction x || isAttrs x;
+      merge = _: defs: { imports = getValues defs; };
+    };
+
+    # `lib.types.nullOr`'s merge function requires definitions
+    # to all be null or all be non-null.
+    #
+    # It was being used where the intent was that null be used as a
+    # value representing unset, and as such the merge should return null
+    # if all definitions are null and ignore nulls otherwise.
+    #
+    # This adds a type with that merge semantics.
+    nullable =
+      elemType:
+      (nullOr elemType)
+      // {
+        name = "nullable";
+        description = "nullable ${
+          optionDescriptionPhrase (class: class == "noun" || class == "composite") elemType
+        }";
+        descriptionClass = "noun";
+        merge =
+          loc: defs:
+          if all (def: def.value == null) defs then
+            null
+          else
+            elemType.merge loc (filter (def: def.value != null) defs);
+        substSubModules = m: nullable (elemType.substSubModules m);
+        functor = (defaultFunctor "nullable") // {
+          type = nullable;
+          wrapped = elemType;
         };
       };
-    }
-  );
 
-  loaders = lazyAttrsOf loader;
+    packageDef = mkOptionType {
+      name = "packageDef";
+      description = "package definition";
+      descriptionClass = "noun";
+      check = isFunction;
+      merge = mergeOneOption;
+    };
 
-  module = mkOptionType {
-    name = "module";
-    description = "module";
-    descriptionClass = "noun";
-    check = x: isPath x || isFunction x || isAttrs x;
-    merge = _: defs: { imports = getValues defs; };
-  };
+    path = types.path // {
+      check = isPath;
+    };
 
-  # `lib.types.nullOr`'s merge function requires definitions
-  # to all be null or all be non-null.
-  #
-  # It was being used where the intent was that null be used as a
-  # value representing unset, and as such the merge should return null
-  # if all definitions are null and ignore nulls otherwise.
-  #
-  # This adds a type with that merge semantics.
-  nullable =
-    elemType:
-    (nullOr elemType)
-    // {
-      name = "nullable";
-      description = "nullable ${
-        optionDescriptionPhrase (class: class == "noun" || class == "composite") elemType
-      }";
+    pathTree = lazyAttrsOf (either path pathTree);
+
+    optCallWith = args: elemType: coercedTo function (x: x args) elemType;
+
+    optFunctionTo =
+      let
+        nonFunction = mkOptionType {
+          name = "nonFunction";
+          description = "non-function";
+          descriptionClass = "noun";
+          check = x: !isFunction x;
+          merge = mergeOneOption;
+        };
+      in
+      elemType: coercedTo nonFunction (x: _: x) (functionTo elemType);
+    optListOf = elemType: coercedTo elemType singleton (listOf elemType);
+
+    outputs = lazyAttrsOf outputsValue;
+
+    outputsValue = mkOptionType {
+      name = "outputs";
+      description = "outputs value";
       descriptionClass = "noun";
       merge =
         loc: defs:
-        if all (def: def.value == null) defs then
-          null
+        if (length defs) == 1 then
+          (head defs).value
+        else if all isAttrs (getValues defs) then
+          (lazyAttrsOf outputsValue).merge loc defs
         else
-          elemType.merge loc (filter (def: def.value != null) defs);
-      substSubModules = m: nullable (elemType.substSubModules m);
-      functor = (defaultFunctor "nullable") // {
-        type = nullable;
-        wrapped = elemType;
-      };
+          throw "The option `${showOption loc}' has conflicting definitions in ${showFiles (getFiles defs)}";
     };
 
-  packageDef = mkOptionType {
-    name = "packageDef";
-    description = "package definition";
-    descriptionClass = "noun";
-    check = isFunction;
-    merge = mergeOneOption;
-  };
+    overlay = mkOptionType {
+      name = "overlay";
+      description = "nixpkgs overlay";
+      descriptionClass = "noun";
+      check = isFunction;
+      merge = _: defs: composeManyExtensions (getValues defs);
+    };
 
-  path = types.path // {
-    check = isPath;
-  };
+    stringLike = mkOptionType {
+      name = "stringLike";
+      description = "string-convertible value";
+      descriptionClass = "noun";
+      check = isStringLike;
+      merge = mergeEqualOption;
+    };
 
-  pathTree = lazyAttrsOf (either path pathTree);
-
-  optCallWith = args: elemType: coercedTo function (x: x args) elemType;
-
-  optFunctionTo =
-    let
-      nonFunction = mkOptionType {
-        name = "nonFunction";
-        description = "non-function";
-        descriptionClass = "noun";
-        check = x: !isFunction x;
-        merge = mergeOneOption;
-      };
-    in
-    elemType: coercedTo nonFunction (x: _: x) (functionTo elemType);
-  optListOf = elemType: coercedTo elemType singleton (listOf elemType);
-
-  outputs = lazyAttrsOf outputsValue;
-
-  outputsValue = mkOptionType {
-    name = "outputs";
-    description = "outputs value";
-    descriptionClass = "noun";
-    merge =
-      loc: defs:
-      if (length defs) == 1 then
-        (head defs).value
-      else if all isAttrs (getValues defs) then
-        (lazyAttrsOf outputsValue).merge loc defs
-      else
-        throw "The option `${showOption loc}' has conflicting definitions in ${showFiles (getFiles defs)}";
-  };
-
-  overlay = mkOptionType {
-    name = "overlay";
-    description = "nixpkgs overlay";
-    descriptionClass = "noun";
-    check = isFunction;
-    merge = _: defs: composeManyExtensions (getValues defs);
-  };
-
-  stringLike = mkOptionType {
-    name = "stringLike";
-    description = "string-convertible value";
-    descriptionClass = "noun";
-    check = isStringLike;
-    merge = mergeEqualOption;
-  };
-
-  template = submodule (
-    { name, ... }:
-    {
-      options = {
-        path = mkOption {
-          type = nullOr path;
-          default = null;
+    template = submodule (
+      { name, ... }:
+      {
+        options = {
+          path = mkOption {
+            type = nullOr path;
+            default = null;
+          };
+          description = mkOption {
+            type = str;
+            default = name;
+          };
+          welcomeText = mkOption {
+            type = nullOr lines;
+            default = null;
+          };
         };
-        description = mkOption {
-          type = str;
-          default = name;
-        };
-        welcomeText = mkOption {
-          type = nullOr lines;
-          default = null;
-        };
-      };
-    }
-  );
+      }
+    );
 
-  test = either (lazyAttrsOf raw) (nonEmptyListOf raw);
-}
+    test = either (lazyAttrsOf raw) (nonEmptyListOf raw);
+  }
+)
--- modules/config/apps.nix
+++ modules/config/apps.nix
@@ -64,36 +64,38 @@ let
 
   appType = parameterize app (
     self': app:
-    (mkOptionType rec {
-      name = "appType";
-      description =
-        let
-          targetDesc = optionDescriptionPhrase (class: class == "noun" || class == "composite") (
-            coercedTo stringLike (abort "") app
-          );
-        in
-        "${targetDesc} or function that evaluates to it";
-      descriptionClass = "composite";
-      check = x: isFunction x || app.check x || stringLike.check x;
-      merge =
-        loc: defs: pkgs:
-        let
-          targetType = coercedTo stringLike (mkApp (last loc) pkgs) app;
-        in
-        (mergeDefinitions loc targetType (
-          map (fn: {
-            inherit (fn) file;
-            value = if isFunction fn.value then fn.value pkgs else fn.value;
-          }) defs
-        )).mergedValue;
-      inherit (app) getSubOptions getSubModules;
-      substSubModules = m: self' (app.substSubModules m);
-      functor = (defaultFunctor name) // {
-        wrapped = app;
-      };
-      nestedTypes.coercedType = stringLike;
-      nestedTypes.finalType = app;
-    })
+    (mkOptionType (
+      fix (self: {
+        name = "appType";
+        description =
+          let
+            targetDesc = optionDescriptionPhrase (class: class == "noun" || class == "composite") (
+              coercedTo stringLike (abort "") app
+            );
+          in
+          "${targetDesc} or function that evaluates to it";
+        descriptionClass = "composite";
+        check = x: isFunction x || app.check x || stringLike.check x;
+        merge =
+          loc: defs: pkgs:
+          let
+            targetType = coercedTo stringLike (mkApp (last loc) pkgs) app;
+          in
+          (mergeDefinitions loc targetType (
+            map (fn: {
+              inherit (fn) file;
+              value = if isFunction fn.value then fn.value pkgs else fn.value;
+            }) defs
+          )).mergedValue;
+        inherit (app) getSubOptions getSubModules;
+        substSubModules = m: self' (app.substSubModules m);
+        functor = (defaultFunctor self.name) // {
+          wrapped = app;
+        };
+        nestedTypes.coercedType = stringLike;
+        nestedTypes.finalType = app;
+      })
+    ))
   );
 in
 {
