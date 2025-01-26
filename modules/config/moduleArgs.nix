e54ecccb27f196db7c31906be7903205aa05102d
Ratson <github@ratson.name>
2025-01-27 03:22:46 +0800

    refactor: move systems releated config

--- modules/config/moduleArgs.nix
+++ modules/config/moduleArgs.nix
@@ -1,42 +1,19 @@
 {
   config,
   lib,
-  conflake,
   ...
 }@args:
 
 let
-  inherit (builtins) mapAttrs;
   inherit (lib)
-    genAttrs
     mkEnableOption
     mkIf
     mkMerge
-    mkOption
-    types
     ;
-  inherit (config) outputs systems;
-  inherit (conflake) selectAttr;
+  inherit (config) outputs;
 
   cfg = config.moduleArgs;
   inputs = config.finalInputs;
-
-  pkgsFor = genAttrs systems (
-    system:
-    import inputs.nixpkgs {
-      inherit system;
-      inherit (config.nixpkgs) config overlays;
-    }
-  );
-
-  genSystems = f: genAttrs systems (system: f pkgsFor.${system});
-
-  mkSystemArgs = system: {
-    inputs' = mapAttrs (_: selectAttr system) inputs;
-    outputs' = selectAttr system outputs;
-  };
-
-  mkSystemArgs' = pkgs: mkSystemArgs pkgs.stdenv.hostPlatform.system;
 in
 {
   options = {
@@ -46,30 +23,6 @@ in
       };
     };
 
-    pkgsFor = mkOption {
-      internal = true;
-      readOnly = true;
-      type = types.unspecified;
-      default = pkgsFor;
-    };
-    genSystems = mkOption {
-      internal = true;
-      readOnly = true;
-      type = types.unspecified;
-      default = genSystems;
-    };
-    mkSystemArgs' = mkOption {
-      internal = true;
-      readOnly = true;
-      type = types.unspecified;
-      default = mkSystemArgs';
-    };
-    mkSystemArgs = mkOption {
-      internal = true;
-      readOnly = true;
-      type = types.unspecified;
-      default = mkSystemArgs;
-    };
   };
 
   config = mkMerge [
--- modules/config/systems.nix
+++ modules/config/systems.nix
@@ -1,20 +1,79 @@
-{ lib, inputs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  conflake,
+  ...
+}:
 
 let
-  inherit (lib) mkOption systems;
+  inherit (builtins) getAttr mapAttrs;
+  inherit (lib)
+    flip
+    genAttrs
+    mkOption
+    types
+    ;
   inherit (lib.types)
     coercedTo
+    functionTo
+    lazyAttrsOf
     listOf
     nonEmptyStr
     package
     uniq
     ;
+  inherit (conflake) selectAttr;
+
+  cfg = config.systems;
+
+  genSystems = f: genAttrs cfg (system: f config.pkgsFor.${system});
+
+  mkSystemArgs = system: {
+    inputs' = mapAttrs (_: selectAttr system) inputs;
+    outputs' = selectAttr system config.outputs;
+  };
+
+  mkSystemArgs' = pkgs: mkSystemArgs pkgs.stdenv.hostPlatform.system;
 in
 {
   options = {
     systems = mkOption {
       type = coercedTo package import (uniq (listOf nonEmptyStr));
-      default = inputs.systems or systems.flakeExposed;
+      default = inputs.systems or lib.systems.flakeExposed;
+    };
+
+    pkgsFor = mkOption {
+      internal = true;
+      readOnly = true;
+      type = coercedTo (functionTo types.pkgs) (v: genAttrs cfg (flip getAttr v)) (
+        lazyAttrsOf types.pkgs
+      );
+      default = genAttrs cfg (
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          inherit (config.nixpkgs) config overlays;
+        }
+      );
+    };
+    genSystems = mkOption {
+      internal = true;
+      readOnly = true;
+      type = types.unspecified;
+      default = genSystems;
+    };
+    mkSystemArgs' = mkOption {
+      internal = true;
+      readOnly = true;
+      type = types.unspecified;
+      default = mkSystemArgs';
+    };
+    mkSystemArgs = mkOption {
+      internal = true;
+      readOnly = true;
+      type = types.unspecified;
+      default = mkSystemArgs;
     };
   };
 }
