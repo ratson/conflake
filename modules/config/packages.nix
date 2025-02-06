ed39f5cad190bb8c08202acd881d05754c21d7e2
Ratson <github@ratson.name>
2025-02-06 21:06:42 +0800

    refactor: move config.packageOverlay

--- modules/config/packages.nix
+++ modules/config/packages.nix
@@ -2,25 +2,12 @@
   config,
   lib,
   conflake,
-  moduleArgs,
   ...
 }:
 
 let
-  inherit (builtins)
-    isList
-    hasAttr
-    mapAttrs
-    parseDrvName
-    tryEval
-    ;
+  inherit (builtins) isList mapAttrs;
   inherit (lib)
-    defaultTo
-    filterAttrs
-    findFirst
-    flip
-    functionArgs
-    mergeAttrs
     mkIf
     mkMerge
     mkOption
@@ -35,39 +22,11 @@ let
     listOf
     oneOf
     str
-    uniq
     ;
   inherit (conflake) callWith;
-  inherit (conflake.internal) nameMockedPkgs;
-  inherit (conflake.types) nullable overlay;
+  inherit (conflake.types) nullable;
 
   cfg = config.packages;
-
-  genPkg =
-    final: prev: name: pkg:
-    let
-      args = functionArgs pkg;
-      noArgs = args == { };
-      pkg' = if noArgs then { pkgs }: pkg pkgs else pkg;
-      dependsOnSelf = hasAttr name args;
-      dependsOnPkgs = noArgs || (args ? pkgs);
-      selfOverride = {
-        ${name} = prev.${name} or (throw "${name} depends on ${name}, but no existing ${name}.");
-      };
-      overrides =
-        filterAttrs (k: _: hasAttr k args) moduleArgs
-        // optionalAttrs dependsOnSelf selfOverride
-        // optionalAttrs dependsOnPkgs { pkgs = final.pkgs // selfOverride; };
-    in
-    final.callPackage pkg' overrides;
-  genPkgs =
-    final: prev: pkgs:
-    mapAttrs (name: genPkg final prev name) pkgs;
-
-  getPkgDefs = flip pipe [
-    config.mkSystemArgs'
-    ({ pkgsCall, ... }: pkgsCall cfg)
-  ];
 in
 {
   options = {
@@ -137,11 +96,6 @@ in
         packages'
       );
     };
-    packageOverlay = mkOption {
-      internal = true;
-      type = uniq overlay;
-      default = _: _: { };
-    };
   };
 
   config = mkMerge [
@@ -150,30 +104,6 @@ in
     })
 
     (mkIf (cfg != null) {
-      packageOverlay =
-        final: prev:
-        let
-          pkgDefs = getPkgDefs prev;
-          getName = pkg: pkg.pname or (parseDrvName pkg.name).name;
-          mockPkgs = nameMockedPkgs prev;
-
-          defaultPkgName = flip defaultTo config.pname (
-            findFirst (x: (tryEval x).success) null [
-              (getName (mockPkgs.callPackage pkgDefs.default { }))
-            ]
-          );
-          default = genPkg final prev defaultPkgName pkgDefs.default;
-        in
-        pipe pkgDefs [
-          (genPkgs final prev)
-          (mergeAttrs (
-            optionalAttrs (pkgDefs ? default && defaultPkgName != null) {
-              inherit default;
-              ${defaultPkgName} = default;
-            }
-          ))
-        ];
-
       outputs.packages = config.finalPackages;
 
       devShell.inputsFrom =
